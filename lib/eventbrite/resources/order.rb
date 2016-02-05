a9fe7fd19c26aad0496bb3103b6cca4e025ef6bb
Chris Wong <chwongris@gmail.com>
2016-02-05 18:31:10 -0500

    Add parameter

--- lib/eventbrite/resources/order.rb
+++ lib/eventbrite/resources/order.rb
@@ -1,4 +1,9 @@
 module Eventbrite
   class Order < APIArrayResource
+     def self.retrieve(user_id, params={}, token=nil)
+      instance = self.new(id, token, params)
+      instance.refresh
+      instance
+    end
   end
 end
