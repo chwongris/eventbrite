9f39cde798494a9a071eb0aa01a834a841bcf95b
Chris Wong <chwongris@gmail.com>
2016-02-05 19:27:15 -0500

    Order retrieve options

--- lib/eventbrite/resources/order.rb
+++ lib/eventbrite/resources/order.rb
@@ -1,14 +1,10 @@
 module Eventbrite
-
   class Order < APIArrayResource
-    def self.url
-      "/orders"
-    end
-
-    def self.retrieve(id, params={}, token=nil)
-      response, token = Eventbrite.request(:get, url, token, params)
-      Util.convert_to_eventbrite_object(response, token)
+     def self.retrieve(id, params={}, token=nil)
+      instance = self.new(id, token)
+      instance.retrieve_options = params
+      instance.refresh
+      instance
     end
   end
-
 end
