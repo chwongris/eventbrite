4f8b9c5762a8d2fe5af0859d1304bc037e1df9fe
Chris Wong <chwongris@gmail.com>
2016-08-08 18:25:37 -0400

    Access code creation

--- lib/eventbrite/resources/access_code.rb
+++ lib/eventbrite/resources/access_code.rb
@@ -1,4 +1,8 @@
 module Eventbrite
   class AccessCode < APIArrayResource
+  	 def self.create(body, token=nil)
+      response, token = Eventbrite.request(:post, url, token, body)
+      Util.convert_to_eventbrite_object(response, token)
+    end
   end
 end
