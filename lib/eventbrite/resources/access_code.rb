4d866fccc5e9d26747f0ffb109d181131a5e25b3
Chris Wong <chwongris@gmail.com>
2016-08-16 15:04:24 -0400

    Invite Code Create

--- lib/eventbrite/resources/access_code.rb
+++ lib/eventbrite/resources/access_code.rb
@@ -1,14 +1,12 @@
 module Eventbrite
   class AccessCode < APIArrayResource
-    def self.create(body, token=nil)
-      response, token = Eventbrite.request(:post, url.access_code_url, token, body)
+    def self.create(params, token=nil)
+      unless event_id = params.delete(:event_id)
+        raise InvalidRequestError.new('No event_id provided.')
+      end
+
+      response, token = Eventbrite.request(:post, self.all_url(event_id), token, params)
       Util.convert_to_eventbrite_object(response, token)
     end
-  end
-
-  private
-
-  def self.access_code_url
-    url + '/access_codes'
   end
 end
